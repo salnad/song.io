fed394d7b3e8b55d89da25f19f3f8f8f5d34da7a
Salil Nadkarni <salnad02@gmail.com>
2020-08-02 17:17:33 -0400

    Change script / index to work

--- public/script.js
+++ public/script.js
@@ -3,6 +3,12 @@ async function act() {
   let playlist_id = parse_playlist_for_id(playlist);
   let res = await fetch(`/playlist?playlist_id=${playlist_id}`);
   let simplified_playlist = await res.json();
+  let random_track =
+    simplified_playlist[Math.floor(Math.random() * simplified_playlist.length)];
+  let audio_src = random_track.thirty_second_url;
+  var audio = new Audio(audio_src);
+  audio.play();
+  document.getElementById("current-song").innerText = random_track.track_title;
   console.log(simplified_playlist);
 }
 
