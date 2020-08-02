088e7e2621a823fbbfe8b9c00488acbf08e9dc20
Salil Nadkarni <salnad02@gmail.com>
2020-08-02 16:47:24 -0400

    Change index.js to return a simplified representation of a playlist given a playlist_id as a query parameter when given a GET to the /playlist endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const CLIENT_ID = "907a3b6dfb7144029e11aa5ac4b68bbf";
 const CLIENT_SECRET = "1a603f6afd374387aa6c3f5bf97882ff";
@@ -11,23 +12,16 @@ app.listen(3000, () => {
   console.log("Server started");
 });
 
-app.get("/token", async (req, res) => {
+app.get("/playlist", async (req, res) => {
   // TODO: @salnad Buffer() is deprecated
   let buffer = new Buffer(CLIENT_ID + ":" + CLIENT_SECRET);
   let authorization = buffer.toString("base64");
 
-  // let authOptions = {
-  //   method: "post",
-  //   url: "https://accounts.spotify.com/api/token",
-  //   form: {
-  //     grant_type: "client_credentials",
-  //   },
-  //   json: true,
-  // };
+  let playlist_id = req.query.playlist_id;
 
-  // // Send a POST request
-  axios
-    .post(
+  // Send a POST request
+  try {
+    let access_token_response = await axios.post(
       "https://accounts.spotify.com/api/token",
       "grant_type=client_credentials",
       {
@@ -35,19 +29,38 @@ app.get("/token", async (req, res) => {
           Authorization: "Basic " + authorization,
         },
       }
-    )
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log("OH NOES!!!");
-    });
-
-  // axios.post(authOptions, function (error, response, body) {
-  //   if (!error && response.statusCode === 200) {
-  //     var token = body.access_token;
-  //     console.log(token);
-  //   }
-  // });
-  res.send("HELLO WORLD!");
+    );
+    let access_token = access_token_response.data.access_token;
+    let spotify_data = await get_playlist_items(access_token, playlist_id);
+    res.send(spotify_data);
+  } catch (err) {
+    console.log(`/token post usage error: ${err}`);
+  }
 });
+
+async function get_playlist_items(access_token, playlist_id) {
+  let spotify_api = new SpotifyWebApi();
+  spotify_api.setAccessToken(access_token);
+  try {
+    let spotify_response = await spotify_api.getPlaylist(playlist_id);
+    let playlist_items = spotify_response.body.tracks.items;
+    let playlist_items_simplified = playlist_items.map((item) => {
+      let track = item.track;
+      if (!track) {
+        return {};
+      }
+      let track_simplified = {
+        track_title: track.name,
+        thirty_second_url: track.preview_url,
+        track_id: track.id,
+        artists: track.artists.map((artist) => {
+          return artist.name;
+        }),
+      };
+      return track_simplified;
+    });
+    return playlist_items_simplified;
+  } catch (err) {
+    console.log(`SpotifyWebApi usage error: ${err}`);
+  }
+}
--- public/script.js
+++ public/script.js
@@ -1,17 +1,6 @@
-let authorization = atob(CLIENT_ID + ":" + CLIENT_SECRET);
-console.log(authorization);
 async function act() {
-  let authorization = atob(CLIENT_ID + ":" + CLIENT_SECRET);
-  let res = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    body: {
-      grant_type: "client_credentials",
-    },
-    header: {
-      Authorization: `Basic ${authorization}`,
-    },
-  });
+  let res = await fetch("/token");
   console.log(res);
-  console.log(res.body);
-  console.log("HELLO");
+  let token = await res.json();
+  console.log(token);
 }
